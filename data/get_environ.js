6a8a13c993dc84bb32457137a59e502c2ccb0fb3
Yogesh Mundada <yogesh@lawn-128-61-21-148.lawn.gatech.edu>
2013-04-08 16:38:53 -0400

    changes

--- data/get_environ.js
+++ data/get_environ.js
@@ -1,5 +1,5 @@
 
 var environ = voodoo.ua();
 
-//console.log("Here here in get_environ: " + JSON.stringify(environ));
+console.log("Here here in get_environ: " + JSON.stringify(environ));
 self.port.emit("got_environ", environ);
